143b7e49c15d5a40e53604fc58c73f68ba75aba0
r-best <bobbyisbest3@gmail.com>
2022-11-11 15:22:31 -0500

    Polly takes 20s to upload the file to s3, I'll do it myself instead

--- polly.py
+++ polly.py
@@ -1,21 +1,32 @@
 import os
 import boto3
+import uuid
+
+from typing import Union
 
 S3_BUCKET=os.environ['S3_Bucket']
 polly = boto3.client('polly')
+s3 = boto3.client('s3')
 
-def getMP3(word: str) -> None:
+def getMP3(word: str) -> Union[str, int]:
     try:
-        response = polly.start_speech_synthesis_task(
+        response = polly.synthesize_speech(
             Engine='neural',
             OutputFormat='mp3',
-            OutputS3BucketName=S3_BUCKET,
             TextType='ssml',
             Text=f'<phoneme alphabet="ipa" ph="{word}" />',
             VoiceId='Joanna'
         )
         print(response)
-        return response["SynthesisTask"]["OutputUri"]
+
+        s3_key = f"{uuid.uuid4()}.mp3"
+        s3.put_object(
+            Body=response['AudioStream'].read(),
+            Bucket=S3_BUCKET,
+            Key=s3_key
+        )
+
+        return f"https://s3.us-east-1.amazonaws.com/phonetics-generator-output/{s3_key}"
     except Exception as e:
         print(e)
         return -1
